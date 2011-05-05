ddcdec8810b971e67805c0b122c98b7746da7b7e
Matt Hackett <richtaur@gmail.com>
2011-05-05 01:09:56 -0700

    more automation options and playing with water making grass burn slower

--- demos/cellular_automation/cellular_automation.js
+++ demos/cellular_automation/cellular_automation.js
@@ -5,10 +5,14 @@ var TYPE_GRASS = 1;
 var TYPE_FIRE = 2;
 var TYPE_WATER = 3;
 
+var LIMIT_FIRE = 5000;
+var LIMIT_DEAD = 30000;
+
 var cells = [];
 var ctx;
 var interval;
 var showBurn;
+var showFade;
 var stage;
 
 var Cell = function (conf) {
@@ -47,7 +51,21 @@ var affectNeighbors = function (predator, elapsed) {
 		getCellAt(predator.x + 1, predator.y)._burn += elapsed;
 		getCellAt(predator.x, predator.y - 1)._burn += elapsed;
 		getCellAt(predator.x, predator.y + 1)._burn += elapsed;
+	} else if (predator.type == TYPE_WATER) {
+		getCellAt(predator.x - 1, predator.y)._burn -= (elapsed / 4);
+		getCellAt(predator.x + 1, predator.y)._burn -= (elapsed / 4);
+		getCellAt(predator.x, predator.y - 1)._burn -= (elapsed / 4);
+		getCellAt(predator.x, predator.y + 1)._burn -= (elapsed / 4);
 	}
+};
+
+var hasFire = function () {
+	for (var i = 0, cell; cell = cells[i]; ++i) {
+		if (cell.type == TYPE_FIRE) {
+			return true;
+		}
+	}
+	return false;
 };
 
 var init = function () {
@@ -78,9 +96,17 @@ var render = function (ctx, elapsed) {
 	var gridSize = 30;
 
 	ctx.save();
-	ctx.fillRect();
+	ctx.fillStyle = "rgb(200, 200, 200)";
+	ctx.fillRect(0, 0, 295, 295);
+	ctx.restore();
 
 	for (var i = 0, cell; cell = cells[i]; ++i) {
+		ctx.save();
+		ctx.shadowBlur = 5;
+		ctx.shadowColor = "rgba(0, 0, 0, 0.75)";
+		ctx.shadowOffsetX = 2;
+		ctx.shadowOffsetY = 2;
+
 		switch (cell.type) {
 			case TYPE_DEAD:
 				ctx.fillStyle = "rgb(0, 0, 0)";
@@ -97,12 +123,29 @@ var render = function (ctx, elapsed) {
 		}
 
 		ctx.fillRect(cell.x * gridSize, cell.y * gridSize, cellSize, cellSize);
+		ctx.restore();
+
+		if (showFade.checked && (cell._burn > 0)) {
+			ctx.save();
+			if (cell.type == TYPE_GRASS) {
+				ctx.fillStyle = "rgb(200, 0, 0)";
+				ctx.globalAlpha = (cell._burn / LIMIT_FIRE);
+				ctx.fillRect(cell.x * gridSize, cell.y * gridSize, cellSize, cellSize);
+			} if (cell.type == TYPE_FIRE) {
+				ctx.fillStyle = "rgb(0, 0, 0)";
+				ctx.globalAlpha = (cell._burn / LIMIT_DEAD);
+				ctx.fillRect(cell.x * gridSize, cell.y * gridSize, cellSize, cellSize);
+			}
+			ctx.restore();
+		}
 
 		if (showBurn.checked) {
+			ctx.save();
 			ctx.fillStyle = "rgb(255, 255, 255)";
 			ctx.font = "8px arial";
 			ctx.textBaseline = "top";
 			ctx.fillText(Math.round(cell._burn), cell.x * gridSize, cell.y * gridSize);
+			ctx.restore();
 		}
 	}
 
@@ -115,27 +158,36 @@ var restart = function () {
 	init();
 };
 
+var stop = function () {
+	clearInterval(interval);
+};
+
 var update = function (elapsed) {
 	for (var i = 0, cell; cell = cells[i]; ++i) {
 		affectNeighbors(cell, elapsed);
 
 		if (cell.type == TYPE_GRASS) {
-			if (cell._burn >= 5000) {
+			if (cell._burn >= LIMIT_FIRE) {
 				cell.type = TYPE_FIRE;
 			}
 		} else if (cell.type == TYPE_FIRE) {
 			cell._burn += elapsed;
-			if (cell._burn >= 30000) {
+			if (cell._burn >= LIMIT_DEAD) {
 				cell.type = TYPE_DEAD;
 			}
 		}
 	}
+
+	if (!hasFire()) {
+		stop();
+	}
 };
 
-stage = document.getElementById("stage");
-showBurn = document.getElementById("show_burn");
-ctx = stage.getContext("2d");
 document.getElementById("restart").addEventListener("click", restart, false);
+showBurn = document.getElementById("show_burn");
+showFade = document.getElementById("show_fade");
+stage = document.getElementById("stage");
+ctx = stage.getContext("2d");
 
 init();
 
